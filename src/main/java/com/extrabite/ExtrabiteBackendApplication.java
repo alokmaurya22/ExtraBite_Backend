ed9fe2243f5f36c91f572f7d4c7a573c9a80b399
Alok Maurya <alokmauryaraj2003@gmail.com>
2025-06-19 00:38:10 +0530

    Project Setup Complete

---
+++ src/main/java/com/extrabite/ExtrabiteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.extrabite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtrabiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtrabiteBackendApplication.class, args);
+	}
+
+}
